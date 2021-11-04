8bd2ab62a8eeb5f4152bf508ac2a0dae997f5310
Sebastian <s.albertogamarro5@gmail.com>
2021-11-04 15:25:17 -0600

    saved changes

---
+++ app.js
@@ -0,0 +1,6 @@
+function calculateDogAge(num) {
+    var degree = document.getElementById("dog").value;
+    var tempCalc = degree*7;
+    document.getElementById("celDeg").innerHTML = Math.round(tempCalc);
+    } 
+    calculateDogAge();
