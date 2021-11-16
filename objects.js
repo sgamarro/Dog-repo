f5043849ae0e0da76a54c31f7a2bb87db80f9bd0
Sebastian <s.albertogamarro5@gmail.com>
2021-11-15 22:14:35 -0700

    saved changes

--- objects.js
+++ objects.js
@@ -1,12 +1,10 @@
-
 var xbox ={
-Manufacture: "Microsoft",
-Memory:"1TB",
-RAM:"16 GB",
-FPS:"120fps",
-Graphics:"AMD RDNA graphics "
-};
-
+  Manufacture: "Microsoft",
+  Memory:"1TB",
+  RAM:"16 GB",
+  FPS:"120fps",
+  Graphics:"AMD RDNA graphics "
+  };
 var ps5 ={
     Manufacture: "Sony",
     Memory:"825 GB",
@@ -22,7 +20,33 @@ var swit ={
     FPS:"60fps",
     Graphics:"NVIDIA GPU"
 };
+
+
 function xboxSpec(){
-document.getElementById('xbox').innerHTML=xbox;
+ document.getElementById('xbox').innerHTML = JSON.stringify(xbox);
+ var x = document.getElementById("xbox");
+ if (x.style.display === "none") {
+   x.style.display = "block";
+ } else {
+   x.style.display = "none";
+ }
 }
 xboxSpec();
+function ps5Spec(){
+  document.getElementById('ps5').innerHTML = JSON.stringify(ps5);
+  var x = document.getElementById("ps5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+ }
+ function switSpec(){
+  document.getElementById('swit').innerHTML = JSON.stringify(swit);
+  var x = document.getElementById("swit");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+ }
