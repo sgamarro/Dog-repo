a5ff20970fa42907c9e3c85dcea5d1c6008eff9e
Sebastian <s.albertogamarro5@gmail.com>
2021-11-16 18:15:24 -0700

    saved changes

--- objects.js
+++ objects.js
@@ -23,30 +23,69 @@ var swit ={
 
 
 function xboxSpec(){
- document.getElementById('xbox').innerHTML = JSON.stringify(xbox);
- var x = document.getElementById("xbox");
- if (x.style.display === "none") {
-   x.style.display = "block";
- } else {
-   x.style.display = "none";
- }
+ document.getElementById('xbox').innerHTML = `Manufacture: ${xbox["Manufacture"]}<br>
+  Memory: ${xbox['Memory']}<br>
+  RAM: ${xbox['RAM']}<br>
+  FPS:${xbox['FPS']}<br>
+  Graphics:${xbox['Graphics']}`;
+//  var x = document.getElementById("xbox");
+//  if (x.style.display === "none") {
+//    x.style.display = "block";
+//  } else {
+//    x.style.display = "none";
+//  }
 }
-xboxSpec();
+
 function ps5Spec(){
-  document.getElementById('ps5').innerHTML = JSON.stringify(ps5);
-  var x = document.getElementById("ps5");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+  document.getElementById('ps5').innerHTML = `Manufacture: ${ps5["Manufacture"]}<br>
+  Memory: ${ps5['Memory']}<br>
+  RAM: ${ps5['RAM']}<br>
+  FPS:${ps5['FPS']}<br>
+  Graphics:${ps5['Graphics']}`;
+  // var x = document.getElementById("ps5");
+  // if (x.style.display === "none") {
+  //   x.style.display = "block";
+  // } else {
+  //   x.style.display = "none";
+  // }
  }
  function switSpec(){
-  document.getElementById('swit').innerHTML = JSON.stringify(swit);
-  var x = document.getElementById("swit");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+  document.getElementById('swit').innerHTML = `Manufacture: ${swit["Manufacture"]}<br>
+  Memory: ${swit['Memory']}<br>
+  RAM: ${swit['RAM']}<br>
+  FPS:${swit['FPS']}<br>
+  Graphics:${swit['Graphics']}`;
+  // var x = document.getElementById("swit");
+  // if (x.style.display === "none") {
+  //   x.style.display = "block";
+  // } else {
+  //   x.style.display = "none";
+  // }
  }
+
+//  var books =[
+//    {
+//      tittle:"Rich dad poor Dad",
+//      author:"Robert Kiyosaki",
+//      alreadyRead: true
+//       },
+//       {
+//         title: "The Power of Now",
+//         author: "Eckhart",
+//         alreadyRead: false
+//       },
+//       {
+//         title:"You are a badass",
+//         author:"skm",
+//         alreadyRead:true
+//       }
+//  ];
+// for (i=0;i<books.length;i++){
+// var books = books [i];
+// var bookInfo= books.title + 'by' + books.author;
+// if(books.alreadyRead){
+//   document.write('You read' + bookInfo);
+// }else {
+//   document.write('you still need to check out' + bookInfo + '<br>');
+// }
+// }
